34ef21846eaccc77ffa39d999aa6f06d9133c5a1
MOUSTAFA EZZAT <moustafaelzan6@gmail.com>
2025-04-22 14:41:17 +0200

    Videos4

--- Store.Application.Api/Middlewares/GlobalErrorHandlingMiddleware.cs
+++ Store.Application.Api/Middlewares/GlobalErrorHandlingMiddleware.cs
@@ -19,6 +19,17 @@ namespace Store.Application.Api.Middlewares
             try
             {
                 await _next.Invoke(context);
+                if (context.Response.StatusCode == StatusCodes.Status404NotFound)
+                { 
+                    context.Response.ContentType = "application/json";
+                    var response = new ErrorDetails()
+                    {
+                        StatusCode = StatusCodes.Status404NotFound,
+                        ErrorMessage = $"End Point {context.Request.Path} is Not Found "
+                    };
+                     await context.Response.WriteAsJsonAsync(response);
+
+                };
             }
             catch (Exception ex)
             {
