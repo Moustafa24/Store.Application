8980a075751bd4843c4cc8baffcdf1ae048b58d9
MOUSTAFA EZZAT <moustafaelzan6@gmail.com>
2025-04-24 23:14:56 +0200

    videos04

---
+++ Core/Services/ChacheService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Contracts;
+using Services.Abstractions;
+
+namespace Services
+{
+    public class ChacheService(ICacheRepository cacheRepository) : ICacheService
+    {
+        public async Task<string?> GetCacheValueAsync(string Key)
+        {
+            var value = await cacheRepository.GetAsync(Key);
+            return value == null ? null : value; 
+
+        }
+
+        public async Task SetCacheValueAsync(string key, string value, TimeSpan duration)
+        {
+            await cacheRepository.SetAsync(key, value,duration);
+        }
+    }
+}
--- Core/Services/ServicesManager.cs
+++ Core/Services/ServicesManager.cs
@@ -6,11 +6,14 @@ namespace Services
 {
     public class ServicesManager(IUnitOfWork unitOfWork, 
         IMapper mapper,
-        IBasketRepository basketRepository
+        IBasketRepository basketRepository,
+        ICacheRepository cacheRepository 
         ) : IServiceManager
     {
         public IProductService ProductService { get; } = new ProductService(unitOfWork, mapper);
 
         public IBasketSrvice BasketSrvice { get; } = new BasketService(basketRepository, mapper);
+
+        public ICacheService CacheService { get; } = new ChacheService(cacheRepository);
     }
 }
