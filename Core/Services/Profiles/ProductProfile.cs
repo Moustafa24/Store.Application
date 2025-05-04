3a7862b98a93b011a8fbd5c6b58bafbe85dd7771
MOUSTAFA EZZAT <moustafaelzan6@gmail.com>
2025-05-04 15:33:28 +0300

    Videos09

---
+++ Core/Services/Profiles/ProductProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Domain.Models;
+using Shared;
+
+namespace Services.Profiles
+{
+    public class ProductProfile : Profile
+    {
+        public ProductProfile()
+        {
+            CreateMap<Product, ProductResultDto>()
+                .ForMember(d => d.BrandName, o => o.MapFrom(s => s.ProductBrand.Name))
+                .ForMember(d => d.TypeName, o => o.MapFrom(s => s.ProductType.Name))
+                //.ForMember(d=>d.PictureUrl , o=>o.MapFrom(s=>$"/{s.PictureUrl}"));
+                .ForMember(d => d.PictureUrl, o => o.MapFrom<PictureUrlResolver>());
+            CreateMap<ProductBrand, BrandResultDto>();
+            CreateMap<ProductType, TypeResultDto>();
+        }
+    }
+}
